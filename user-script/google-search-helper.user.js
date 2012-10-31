21b9677314222e28c9205835fcbf933424cc6a5d
jiguang <jiguang1984@gmail.com>
2012-10-31 13:39:56 +0800

    rename

---
+++ user-script/google-search-helper.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        google-helper
+// @namespace   google
+// @description 当你的网络环境只能访问Google首页或者搜索结果页时，可以用该脚本去掉跳转链接，使搜索结果直接链接到目标网页
+// @author      jiguang
+// @mail        jiguang1984#gmail.com
+// @include     http://www.google.com/*
+// @include     http://www.google.com.hk/*
+// @include     http://www.g.cn/*
+// @grant       none
+// @version     1.0
+// ==/UserScript==
+
+// get all links in search results
+var links = document.getElementById('ires').getElementsByTagName('a');
+
+// add target _blank and remove event handler
+for(var i = 0, j = links.length; i<j; i++){
+    links[i].setAttribute('target', '_blank');  // depends on your need
+    links[i].removeAttribute('onmousedown');
+}
