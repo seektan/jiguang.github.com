195d39d6f952d808e8f91a023265c8286bfab1f1
jiguang <jiguang1984@gmail.com>
2012-10-31 13:33:13 +0800

    xx

--- user-script/google-helper.user.js
+++ user-script/google-helper.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name        google-helper
 // @namespace   google
-// @description help somebody visit google search results
+// @description 当你的网络环境只能访问Google首页或者搜索结果页时，可以用该脚本去掉跳转链接，使搜索结果直接链接到目标网页
+// @author      jiguang
+// @mail        jiguang1984#gmail.com
 // @include     http://www.google.com/*
 // @include     http://www.google.com.hk/*
 // @include     http://www.g.cn/*
